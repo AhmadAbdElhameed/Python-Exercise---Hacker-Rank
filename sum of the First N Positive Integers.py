f3cb0e417f0190465d10ea5e8d6abf75c67c65dd
Ahmad Mohammed Abd Elhameed <abdelhameed9@yahoo.com>
2021-07-23 22:44:30 +0200

    Adding ex for calculate sum of N numbers

---
+++ sum of the First N Positive Integers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 18 14:25:58 2021
+
+@author: Ahmad Abd Elhameed
+"""
+
+n = int(input("Enter your number : "))
+
+sum = (n * (n+1)) / 2
+
+print(sum)
