9169a32e829759afb34d0712d525113faa146ab7
Ahmad Mohammed Abd Elhameed <abdelhameed9@yahoo.com>
2021-07-26 23:04:29 +0200

    Adding ex to calculate time

---
+++ calculate_time.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 18 14:30:51 2021
+
+@author: Ahmad Abd Elhameed
+"""
+
+import time
+def myFunc():
+    start_time = time.time()
+    s = 0
+    for i in range(1,n+1):
+        s += 1
+    end_time = time.time()
+    
+    return s,end_time - start_time
+
+n = 1500
+
+print(myFunc)
